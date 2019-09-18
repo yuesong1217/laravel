38de38df6794577741d07cee6d0658bc695c1e82
yuesong1217 <yuesong1217@163.com>
2019-09-18 16:28:08 +0800

    laravel

--- app/Http/Controllers/wechat/MenuController.php
+++ app/Http/Controllers/wechat/MenuController.php
@@ -8,12 +8,12 @@ use Illuminate\Support\Facades\Redis;
 use App\Tools\tools;
 class MenuController extends Controller
 {
-    public $tools;
+    // public $tools;
 
-    public function __construct(Tools $tools)
-    {
-        $this->tools = $tools;
-    }
+    // public function __construct(Tools $tools)
+    // {
+    //     $this->tools = $tools;
+    // }
 
     public function create_menu()
     {
@@ -47,7 +47,15 @@ class MenuController extends Controller
 
     public function event()
     {
-        echo $_GET['echostr'];
+        $xml_string = file_get_contents('php://input');  //获取
+        $wechat_log_psth = storage_path('logs/wechat/'.date('Y-m-d').'.log');
+        file_put_contents($wechat_log_psth,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+        file_put_contents($wechat_log_psth,$xml_string,FILE_APPEND);
+        file_put_contents($wechat_log_psth,"\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n",FILE_APPEND);
+        //dd($xml_string);
+        $xml_obj = simplexml_load_string($xml_string,'SimpleXMLElement',LIBXML_NOCDATA);
+        $xml_arr = (array)$xml_obj;
+        \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
     }
 
 }
