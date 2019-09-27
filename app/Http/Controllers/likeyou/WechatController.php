3636f3a268c6a5ede795c567932d9e4a5edc4a59
yuesong1217 <yuesong1217@163.com>
2019-09-27 14:39:39 +0800

    laravel

--- app/Http/Controllers/likeyou/WechatController.php
+++ app/Http/Controllers/likeyou/WechatController.php
@@ -270,24 +270,11 @@ class WechatController extends Controller
         $xml_arr = (array)$xml_obj;
         \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
         //获取用户的基本信息
-        if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
-            //关注
-            //opnid拿到用户基本信息
-            $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->tools->get_wechat_access_token().'&openid='.$xml_arr['FromUserName'].'&lang=zh_CN';
-            $user_re = file_get_contents($url);
-            $user_info = json_decode($user_re,1);
-            dd($user_info);
-            //存入数据库
-            $db_user = DB::table("wechat_user")->where(['openid'=>$xml_arr['FromUserName']])->first();
-            // dd($db_user);
-            if(empty($db_user)){
-                //没有数据，存入
-                DB::table("wechat_user")->insert([
-                    'openid'=>$xml_arr['FromUserName'],
-                    'add_time'=>time()
-                ]);
-            }
-            $message = '欢迎'.$user_info['nickname'].'同学，感谢您的关注';
+        if ($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe') {
+            $user_info = file_get_contents('https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->tools->get_wechat_access_token().'&openid='.$xml_arr['FromUserName'].'&lang=zh_CN');
+            // dd($user_info);
+            $user = json_decode($user_info,1);
+            $message = '欢迎'.$user['nickname'].'同学，感谢您的关注';
             $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
             echo $xml_str;
         }
